1471949e61224437c34a18b420c6aa897b878f5a
David Hill <dh366k@us.att.com>
2021-05-29 23:32:33 +0100

    Use URLSearchParams for service

--- src/services/giphyService.js
+++ src/services/giphyService.js
@@ -1,11 +1,16 @@
 import ky from "ky";
 
-const baseUrl = "https://api.giphy.com/v1/gifs/random?api_key=e26089724ab941889d776827bf7c0c32&tag=";
+const baseUrl = "https://api.giphy.com/v1/gifs/random?";
 
-const giphyService = async (searchTerm) => {
-  const resp = await ky.get(baseUrl + searchTerm);
-  const json = await resp.json();
-  return json.data;
+const giphyService = (searchTerm) => {
+  const queryParams = new URLSearchParams({
+    api_key: "e26089724ab941889d776827bf7c0c32",
+    tag: searchTerm,
+  });
+  return ky
+    .get(baseUrl + queryParams.toString())
+    .json()
+    .then((res) => res.data);
 };
 
 export { giphyService };
